0fa6d1e785df2938a56957705e1496a42de0f036
nkosiyazi <gwilinkosiyazi1@gmail.com>
2025-11-21 12:12:05 +0200

    Fixes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
-  trailingSlash: false
+  output: 'export', // If you're using static export
+  trailingSlash: true,
+  distDir: '.next',
+  // Add if using static export
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
